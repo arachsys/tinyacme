92a05b862a6e9cc31735d728f9f96b80ceac4789
Chris Webb <chris@arachsys.com>
2021-12-31 22:33:43 +0000

    Extend tinyacme into acmeproxy

---
+++ acmeproxy.go
@@ -0,0 +1,87 @@
+package main
+
+import "golang.org/x/crypto/acme"
+import "golang.org/x/crypto/acme/autocert"
+import "context"
+import "fmt"
+import "log"
+import "net"
+import "net/http"
+import "net/http/httputil"
+import "os"
+import "os/signal"
+import "sync"
+import "syscall"
+
+func die(status int, format string, args ...interface {}) {
+  fmt.Fprintf(os.Stderr, format + "\n", args...)
+  os.Exit(status)
+}
+
+func main() {
+  if len(os.Args) < 2 {
+    die(64, "Usage: %s HOSTNAME...\n", os.Args[0])
+  }
+
+  manager := &autocert.Manager {
+    Cache: autocert.DirCache("."),
+    HostPolicy: autocert.HostWhitelist(os.Args[1:]...),
+    Prompt: autocert.AcceptTOS,
+  }
+
+  server := &http.Server {
+    ErrorLog: log.New(os.Stdout, "", 0),
+    Handler: &httputil.ReverseProxy {
+      Director: func(request *http.Request) {
+        if host, _, err := net.SplitHostPort(request.Host); err == nil {
+          request.Host = host
+        }
+        request.URL.Scheme, request.URL.Host = "http", request.Host
+        if _, ok := request.Header["User-Agent"]; ok == false {
+          request.Header.Set("User-Agent", "")
+        }
+      },
+      ErrorLog: log.New(os.Stdout, "", 0),
+    },
+    TLSConfig: manager.TLSConfig(),
+  }
+
+  terminate := make(chan os.Signal, 1)
+  signal.Notify(terminate, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+
+  if url := os.Getenv("ACMEURL"); url != "" {
+    manager.Client = &acme.Client {
+      DirectoryURL: url,
+    }
+  }
+
+  hosts := make(map[string]string)
+  for _, name := range os.Args[1:] {
+    addresses, err := net.LookupHost(name)
+    if err != nil {
+      die(1, "Failed to resolve hostname %s", name)
+    }
+    for _, host := range addresses {
+      hosts[host] = host
+    }
+  }
+
+  wait := new(sync.WaitGroup)
+  for host := range hosts {
+    listener, err := net.Listen("tcp", net.JoinHostPort(host, "https"))
+    if err != nil {
+      server.Shutdown(context.Background())
+      wait.Wait()
+      die(1, "Failed to listen for https on %s", host)
+    }
+    wait.Add(1)
+    go func() {
+      server.ServeTLS(listener, "", "")
+      wait.Done()
+    }()
+  }
+
+  <-terminate
+  server.Shutdown(context.Background())
+  wait.Wait()
+}
